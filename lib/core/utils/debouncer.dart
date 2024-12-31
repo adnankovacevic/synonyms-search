520044b9ad14b003d7423379bee62724aa6408d0
adnankovacevic <kovacevic9adnan@gmail.com>
2024-12-31 09:54:55 +0100

    Move Debouncer consturctor to the top

--- lib/core/utils/debouncer.dart
+++ lib/core/utils/debouncer.dart
@@ -2,10 +2,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class Debouncer {
+  Debouncer({this.duration = const Duration(milliseconds: 500)});
+
   final Duration duration;
   Timer? _debounce;
-
-  Debouncer({this.duration = const Duration(milliseconds: 500)});
 
   void call(VoidCallback callback) {
     if (_debounce?.isActive ?? false) _debounce?.cancel();
